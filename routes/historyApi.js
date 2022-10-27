9589f5225ebaf7e69fcdd1913a2344842caf117a
noammery <noammery55@gmail.com>
2022-10-27 18:42:43 +0300

    all secured

--- routes/historyApi.js
+++ routes/historyApi.js
@@ -9,8 +9,14 @@ const cryptr = new Cryptr(`${process.env.MESSAGE_TOKEN_SECRET}`);
 router.post(`/room`, async (req, res, next) => {
   if (req.body) {
     const hashedMessage = cryptr.encrypt(req.body.message);
+    const hashedRoom = cryptr.encrypt(req.body.room);
+    const hashedAuthor = cryptr.encrypt(req.body.author);
+    const hashedTime = cryptr.encrypt(req.body.time);
     const hashedObj = req.body;
     hashedObj.message = hashedMessage;
+    hashedObj.room = hashedRoom;
+    hashedObj.author = hashedAuthor;
+    hashedObj.time = hashedTime;
     History.create(hashedObj)
       .then((data) => res.json(data))
       .catch(next);
@@ -18,24 +24,33 @@ router.post(`/room`, async (req, res, next) => {
 });
 
 router.post(`/gethistory`, async (req, res, next) => {
-  const room = req.body.room;
-  const messages = await History.find({ room: room });
+  const room1 = req.body.room;
+  const messages = await History.find();
+  // console.log(messages);
   for (let i = 0; i < messages.length; i++) {
-    const hashedMessage = cryptr.decrypt(messages[i].message);
-    messages[i].message = hashedMessage;
+    const hashedRoom1 = cryptr.decrypt(messages[i].room);
+    if (hashedRoom1 === room1) {
+      const hashedMessage1 = cryptr.decrypt(messages[i].message);
+      const hashedAuthor1 = cryptr.decrypt(messages[i].author);
+      const hashedTime1 = cryptr.decrypt(messages[i].time);
+      messages[i].message = hashedMessage1;
+      messages[i].author = hashedAuthor1;
+      messages[i].room = hashedRoom1;
+      messages[i].time = hashedTime1;
+    }
   }
   res.send(messages);
 });
 
-router.delete(`/room`, (req, res, next) => {
-  const message = req.body;
-  History.findOneAndDelete({
-    message: message.message,
-    author: message.author,
-    room: message.room,
-  })
-    .then(console.log(`message deleted`))
-    .catch(next);
-});
+// router.delete(`/room`, (req, res, next) => {
+//   const message = req.body;
+//   History.findOneAndDelete({
+//     message: message.message,
+//     author: message.author,
+//     room: message.room,
+//   })
+//     .then(console.log(`message deleted`))
+//     .catch(next);
+// });
 
 module.exports = router;
