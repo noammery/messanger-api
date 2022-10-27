b807bf8dcad39924789d4fdbbdbb8b31db10558d
noammery <noammery55@gmail.com>
2022-10-27 11:48:24 +0300

    working

--- routes/historyApi.js
+++ routes/historyApi.js
@@ -1,20 +1,30 @@
 const express = require(`express`);
 const router = express.Router();
 const History = require(`../modals/history`);
+const Cryptr = require(`cryptr`);
+require(`dotenv`).config();
 
-router.post(`/room`, (req, res, next) => {
+const cryptr = new Cryptr(`${process.env.MESSAGE_TOKEN_SECRET}`);
+
+router.post(`/room`, async (req, res, next) => {
   if (req.body) {
-    History.create(req.body)
+    const hashedMessage = cryptr.encrypt(req.body.message);
+    const hashedObj = req.body;
+    hashedObj.message = hashedMessage;
+    History.create(hashedObj)
       .then((data) => res.json(data))
       .catch(next);
   }
 });
 
-router.post(`/gethistory`, (req, res, next) => {
+router.post(`/gethistory`, async (req, res, next) => {
   const room = req.body.room;
-  History.find({ room: room })
-    .then((data) => res.json(data))
-    .catch(next);
+  const messages = await History.find({ room: room });
+  for (let i = 0; i < messages.length; i++) {
+    const hashedMessage = cryptr.decrypt(messages[i].message);
+    messages[i].message = hashedMessage;
+  }
+  res.send(messages);
 });
 
 router.delete(`/room`, (req, res, next) => {
